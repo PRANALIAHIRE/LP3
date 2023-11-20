cae562b25358cae826243889dd8b87cd2e16ddd7
PRANALIAHIRE <118653725+PRANALIAHIRE@users.noreply.github.com>
2023-11-20 07:46:08 +0530

    Add files via upload

---
+++ recursive.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+n = 10
+result_recursive = fibonacci_recursive(n)
+print(f"Fibonacci Recursive({n}):", result_recursive)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
